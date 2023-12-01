828dfaaac9bcb69be66236fb24536606b79b4984
奕独客(Alone) <46129807+yiduk@users.noreply.github.com>
2023-12-01 13:47:25 +0800

    Update aiSider.vue

--- src/views/mj/aiSider.vue
+++ src/views/mj/aiSider.vue
@@ -40,7 +40,7 @@ const goHome =computed(  () => {
                 </n-tooltip>
             </a>
 
-            <!--<a href="#/draw" @click="st.active='draw'" class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]">
+            <a href="#/draw" @click="st.active='draw'" class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]">
                 <n-tooltip placement="right" trigger="hover">
                   <template #trigger> 
                     <div  class="flex h-full justify-center items-center   py-1 flex-col" :class="[goHome=='draw' ? 'active' : '']">
@@ -50,7 +50,7 @@ const goHome =computed(  () => {
                   </template>
                     AI绘画 Midjourney引擎
                 </n-tooltip>
-            </a> -->
+            </a>
 					
             <!-- <section  class=" router-link-exact-active h-12 w-12 cursor-pointer rounded-xl bg-white duration-300 dark:bg-[#34373c] hover:bg-[#bbb] dark:hover:bg-[#555]"
              >
